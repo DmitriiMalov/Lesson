e82925521ce54dba1089a9d98041ff26d18155e0
DmitriiMalov <aikido72@bk.ru>
2022-08-24 08:53:32 +0300

    Project1.1

---
+++ Progect1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите два числа");
+Console.WriteLine ("Введите число a");
+int a = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Введите число b");
+int b = int.Parse (Console.ReadLine ());
+if (a>b)
+{
+    Console.WriteLine ("a=max");
+}
+else
+{
+    Console.WriteLine ("b=max");  
+}
